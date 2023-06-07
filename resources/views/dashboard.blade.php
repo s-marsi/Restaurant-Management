c279538916c8e36f677b90339da98dd9b642b910
smarsi <soufianemarsi1999@gmail.com>
2023-06-07 21:37:58 +0100

    save with breeze

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <title>Dashboard</title>
+
+</head>
+<body>
+  
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between mx-3">
+    <div>
+        <div class="d-flex flex-column bg-white ">
+          <a class="navbar-brand mb-0" href="/dashboard" style="font-size: 15px; font-weight: 600">Mon Resto</a>
+          <a class="navbar-brand" style="font-size: 15px; font-weight: 600">Serveur : {{Auth::user()->name}}</a>
+        </div>
+    </div>
+    <div >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Dashboard</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Commandes</a>
+          </li>
+      </ul>
+    </div>
+    <div>
+      <button class="btn btn-primary">Nouvelle Commande</button>
+    </div>
+  </nav>
+
+
+  @yield("contenue")
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,8 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('visiteur.plats');
+    return view('welcome');
 });
 
-Route::get("/menu", [CategorieController::class, "index"])->name("menu");
-Route::get("/plats/{categorie}", [CategorieController::class, "plats"])->name("plats_categorie");
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
