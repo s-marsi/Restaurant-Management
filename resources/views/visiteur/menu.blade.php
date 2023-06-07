0ec6f9031fd806fb254fc520270d1cfe45b8128e
smarsi <soufianemarsi1999@gmail.com>
2023-06-07 22:54:42 +0100

    add photos

--- resources/views/visiteur/menu.blade.php
+++ resources/views/visiteur/menu.blade.php
@@ -2,15 +2,16 @@
 
 @section("contenue")
 
-  <div class="container d-flex">
+  <div class="container d-flex mt-3">
       @isset($categories)
       <div class="d-flex justify-content-center flex-wrap">
         @foreach($categories  as $key => $cat)
         <div class="card m-2" style="width: 30rem;">
-          <img class="card-img-top  img" src="{{asset($cat->photo)}}" alt="Card image cap">
+          <img class="card-img-top  img" src="{{asset($cat->photo)}}" alt="{{$cat->titre}} ">
           <div class="card-body">
             <h5 class="card-title">{{$cat->titre}}</h5>
-            <form action="{{route("plats_categorie", $cat)}}">
+            <form action="{{route("plats_categorie", $cat->id)}}" method="get">
+              @csrf
               <button href="#" class="btn btn-primary">Découvrir les {{count($cat->plats)}} plats</button>
             </form>
           </div>
